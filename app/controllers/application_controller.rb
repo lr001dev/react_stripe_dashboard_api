ceba6876229a3fed5f58c3ddb1ab2c14fadda504
Luis Rosario <luisrosariomix@gmail.com>
2019-08-11 19:11:55 -0700

    Some Cleanup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,18 +7,12 @@ class ApplicationController < ActionController::API
   end
 
   def bearer_token
-    # puts "BEARER TOKEN"
-    # puts header = request.env["HTTP_AUTHORIZATION"]
     header = request.env["HTTP_AUTHORIZATION"]
     pattern = /^Bearer /
-    # puts "TOKEN WITHOUT BEARER"
-    # puts header.gsub(pattern, '') if header && header.match(pattern)
     header.gsub(pattern, '') if header && header.match(pattern)
   end
 
   def decode_token(token_input)
-    # puts "DECODE TOKEN, token input: #{ token_input }"
-    # puts token = JWT.decode(token_input, ENV["JWT_SECRET"], true, { :algorithm => 'HS256'})
     jwt = cookies.signed[:jwt]
     JWT.decode(jwt, ENV["JWT_SECRET"], true, { :algorithm => 'HS256'})
   rescue
